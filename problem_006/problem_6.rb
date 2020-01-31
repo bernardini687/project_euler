e8e8956ffdb921788606dc840a6c20e81d690bff
oscar bernardini <bernardini687@gmail.com>
2020-01-31 17:19:22 +0100

    rename ruby solution

--- problem_006/problem_6.rb
+++ problem_006/problem_6.rb
@@ -5,18 +5,21 @@
 module Problem6
   PROBLEM_SIZE = 100
 
-  def self.solution(sqr_of_sum, sum_of_sqrs)
-    sqr_of_sum - sum_of_sqrs
+  def self.solution(size: PROBLEM_SIZE)
+    sqr_of_sum(size) - sum_of_sqrs(size)
   end
 
-  def self.sqr_of_sum(custom_size = PROBLEM_SIZE)
+  def self.sqr_of_sum(custom_size)
     (1..custom_size).reduce(:+).pow(2)
   end
 
-  def self.sum_of_sqrs(custom_size = PROBLEM_SIZE)
+  def self.sum_of_sqrs(custom_size)
     (1..custom_size).reduce { |acc, n| acc + n.pow(2) }
   end
 end
 
-puts Problem6.solution(Problem6.sqr_of_sum, Problem6.sum_of_sqrs)
+puts Problem6.solution
 # => 25_164_150
+
+puts Problem6.solution size: 10
+# => 2_640
